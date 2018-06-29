8326bd75676a2ae48542fb5aca3ac3e19b6b8602
Joel Gutierrez <joelgutz@fb.com>
2018-06-29 15:00:54 -0700

    added details view controller

---
+++ flix/View Controllers/DetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailsViewController.h
+//  flix
+//
+//  Created by Joel Gutierrez on 6/29/18.
+//  Copyright © 2018 Joel Gutierrez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailsViewController : UIViewController
+@property (nonatomic, strong) NSDictionary *movie;
+@end
